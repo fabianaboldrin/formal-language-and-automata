36706dfda378dd771de33cb377642c48a98f8f02
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-02 21:06:36 -0300

    hotfix

--- regular-expressions/TestaMatch.java
+++ regular-expressions/TestaMatch.java
@@ -3,7 +3,7 @@ import java.util.regex.Matcher;
 
 public class TestaMatch{
 	public static void main(String args[]) {
-		Pattern p = Patter.compile("a*b");
+		Pattern p = Pattern.compile("a*b");
 		Matcher m = p.matcher("aaaaaab");
 		boolean b = m.matches();
 	}
