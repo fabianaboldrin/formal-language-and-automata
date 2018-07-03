ac7f9d2c74d49d52d9f91ac32f31301b2d4a7979
Fabiana Boldrin <bibacb.fb@gmail.com>
2018-07-02 21:08:43 -0300

    add args

--- regular-expressions/TestaMatch.java
+++ regular-expressions/TestaMatch.java
@@ -1,11 +1,12 @@
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-public class TestaMatch{
+public class TestaMatch {
 	public static void main(String args[]) {
-		Pattern p = Pattern.compile("a*b");
-		Matcher m = p.matcher("aaaaaab");
+		Pattern p = Pattern.compile(args[0]);
+		Matcher m = p.matcher(args[1]);
 		boolean b = m.matches();
+		
+		System.out.println("Resultado: "+b);
 	}
-	
 }
